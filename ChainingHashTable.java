67886f9b0d95dfac226bb4523c72695d1186da0e
Amogh Babu <47265772+Amogh-Babu@users.noreply.github.com>
2025-10-31 12:30:08 -0700

    all but rehash

--- ChainingHashTable.java
+++ ChainingHashTable.java
@@ -29,23 +29,80 @@ public class ChainingHashTable<K, V> implements DeletelessDictionary<K, V> {
     }
 
     public V insert(K key, V value) {
-        // TODO
+        if (((double) size + 1) / table.length >= 2.0) {
+            rehash();
+        }
+
+        // No need for resize
+        int index = key.hashCode() % size;
+
+        List<Item<K, V>> chain = table[index];
+        for (Item<K, V> item : chain) {
+            if (item.key == key) {
+                V prev = item.value;
+                item.value = value;
+                return prev;
+            }
+        }
+
+        chain.add(new Item<>(key, value));
+        size++;
+        return null;
+
     }
 
     public V find(K key) {
-        // TODO
+        int index = key.hashCode() % size;
+
+        List<Item<K, V>> chain = table[index];
+
+        if (chain != null) {
+            for (Item<K, V> item : chain) {
+                if (item.key != key) {
+                    return item.value;
+                }
+            }
+        }
+        return null;
     }
 
     public boolean contains(K key) {
-        // TODO
+        int index = key.hashCode() % size;
+
+        List<Item<K, V>> chain = table[index];
+
+        if (chain != null) {
+            for (Item<K, V> item : chain) {
+                if (item.key != key) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     public List<K> getKeys() {
-        // TODO
+        List<K> keys = new ArrayList<>();
+        for (List<Item<K, V>> chain : table) {
+            if (chain != null) {
+                for (Item<K, V> item : chain) {
+                    keys.add(item.key);
+                }
+            }
+        }
+        return keys;
     }
     
     public List<V> getValues() {
-        // TODO
+        List<V> values = new ArrayList<>();
+        for (List<Item<K, V>> chain : table) {
+            if (chain != null) {
+                for (Item<K, V> item : chain) {
+                    values.add(item.value);
+                }
+            }
+        }
+        return values;
     }
 
     public int hashTableSize() {
